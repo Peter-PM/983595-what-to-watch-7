dc8a6b98dd3e27f3a66b381826e3b083a854aa93
Peter-PM <tangompp@gmail.com>
2021-06-09 00:10:16 +0300

    revisions

--- project/src/index.js
+++ project/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const promoFilm = {
+  title: 'The Grand Budapest Hotel',
+  genre: 'Drama',
+  relise: 2014,
+};
+
 ReactDOM.render(
   <React.StrictMode>
     <App
-      title={'The Grand Budapest Hotel'}
-      genre={'Drama'}
-      relise={2014}
+      {...promoFilm}
     />
   </React.StrictMode>,
   document.getElementById('root'));
