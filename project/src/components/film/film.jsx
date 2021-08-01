a96311554c7a491bd692f939e09fa88f6079fa9c
Peter-PM <tangompp@gmail.com>
2021-08-01 03:30:13 +0300

    revisions

--- project/src/components/film/film.jsx
+++ project/src/components/film/film.jsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FilmPage from './film-page';
-import { useHistory } from 'react-router';
-import { AppRoute } from '../../const';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
 
 function Film(props) {
-  const history = useHistory();
 
   if (!props.filmFromUrl) {
     return <NotFoundScreen/>;
